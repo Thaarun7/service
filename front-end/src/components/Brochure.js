90913b3e6be3f3693464125c64c54cfaa8e89cd4
Thaarun7 <thaarundevacc0@gmail.com>
2021-05-15 03:50:53 +0530

    commit 3

--- front-end/src/components/Brochure.js
+++ front-end/src/components/Brochure.js
@@ -1,11 +1,26 @@
-import { Card } from '@material-ui/core';
+import { Button, Card } from '@material-ui/core';
 import React from 'react';
 import './Brochure.css';
 
 function Brochure() {
 	return (
 		<div className='card'>
-			<Card>hello</Card>
+			<Card>
+				<div className='card1'>
+					<h1 className='card__heading1'>Gendral Service</h1>
+
+					<div className='details'>
+						<h3>
+							gendral service include with
+							<ol>
+								<li>oil change</li>
+								<li>water service</li>
+							</ol>
+						</h3>
+						<Button className="button">book</Button>
+					</div>
+				</div>
+			</Card>
 		</div>
 	);
 }
